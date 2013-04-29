85fedb1e908289348307a61fefa839f3f57ccb07
Ingo Pfennigstorf <i.pfennigstorf@gmail.com>
2013-04-29 16:39:37 +0200

    Pagination Bug removed

--- js/Gui/Pagination.js
+++ js/Gui/Pagination.js
@@ -1,9 +1,5 @@
 /**
  * Implementation for the pagination panel.
- * Consists of three parts:
- * 	previous link
- * 	current page selector
- * 	next link
  *
  * @constructor
  * @this {Pagination}
@@ -23,85 +19,34 @@ function Pagination(div,pages){
  * @this {Pagination}
  */
 Pagination.prototype.initialize = function(){
-	var pagination = this;
-
-	// add link for previous page navigation
-	var previousPageLi = document.createElement('li');
-
-	var toolsPaginationBackward = document.createElement('a');
-	toolsPaginationBackward.setAttribute('class', 'tools-paginationbackward');
-	toolsPaginationBackward.setAttribute('title', Util.getString('previousPage'));
-
-	var spanVisuallyHidden = document.createElement('span');
-	spanVisuallyHidden.setAttribute('class', 'visuallyhidden');
-
-	toolsPaginationBackward.appendChild(spanVisuallyHidden);
-
-	previousPageLi.appendChild(toolsPaginationBackward);
-
-	$(toolsPaginationBackward).click(function() {
-		if (pagination.page > 1) {
-			pagination.setPage(pagination.page - 1);
+	var pagination = this;	
+	var previousPageLi = $('<li/>').appendTo(this.div);
+	this.previousPage = $('<a class="tools-paginationbackward"><span class="visuallyhidden"/></a>').appendTo(previousPageLi);
+	this.previousPage.title = Util.getString('previousPage');
+	this.previousPage.click(function(){
+		if( pagination.page > 1 ){
+			pagination.setPage(pagination.page-1);
 		}
 	});
-
-	this.div.append(previousPageLi);
-
-	// add select field for selecting or showing the current page number
-	var formLi = document.createElement('li');
-
-	var form = document.createElement('form');
-
-	form.setAttribute('style', 'display:inline');
-
-	var selectPageDropdown = document.createElement('select');
-
-	// iterate over pages
-	for (var i = 0; i < this.pages; i++) {
-		var pageNumber = i + 1;
-		var option = document.createElement('option');
-		option.setAttribute('value', pageNumber);
-
-		var pageNumberText = document.createTextNode(pageNumber);
-		option.appendChild(pageNumberText);
-
-		selectPageDropdown.appendChild(option);
+	var form = $('<form/>').appendTo(this.div);
+	form.css('display','inline');	
+	this.selectPageDropdown = $('<select/>').appendTo(this.div);
+	for( var i=0; i<this.pages; i++ ){
+		$('<option value="'+(i+1)+'">'+(i+1)+'</option>').appendTo(this.selectPageDropdown);
 	}
-
-	$(selectPageDropdown).change(function(evt) {
+	$(this.selectPageDropdown).change(function(evt){
 		pagination.page = parseInt(evt.currentTarget.value);
 		pagination.update();
 	});
-
-	form.appendChild(selectPageDropdown);
-
-	formLi.appendChild(form);
-
-	this.div.append(formLi);
-
-	// add element for next page navigation
-	var nextPageLi = document.createElement('li');
-
-	var toolsPaginationForward = document.createElement('a');
-	toolsPaginationForward.setAttribute('class', 'tools-paginationforward');
-	toolsPaginationForward.setAttribute('title', Util.getString('nextPage'));
-
-	var spanVisuallyHidden = document.createElement('span');
-	spanVisuallyHidden.setAttribute('class', 'visuallyhidden');
-
-	toolsPaginationForward.appendChild(spanVisuallyHidden);
-
-	nextPageLi.appendChild(toolsPaginationForward);
-
-	$(toolsPaginationForward).click(function() {
-		if (pagination.page < pagination.pages) {
+	var nextPageLi = $('<li/>').appendTo(this.div);
+	this.nextPage = $('<a class="tools-paginationforward"><span class="visuallyhidden"/></a>').appendTo(nextPageLi);
+	this.nextPage.title = Util.getString('nextPage');
+	this.nextPage.click(function(){
+		if( pagination.page < pagination.pages ){
 			pagination.page = parseInt(pagination.page + 1);
 			pagination.setPage(pagination.page);
 		}
 	});
-
-	this.div.append(nextPageLi);
-
 }
 
 /**
@@ -112,7 +57,7 @@ Pagination.prototype.initialize = function(){
  * @param {boolean} avoidTrigger If this value is true, the trigger function will not be called (required to avoid infinite loops when views are linked).
  */
 Pagination.prototype.setPage = function(page,avoidTrigger){
-	var pagination = this;
+	var pagination = this;	
 	this.page = page;
 	$('select option:selected',this.selectPageDropdown).removeAttr('selected');
 	$("option",this.selectPageDropdown).each(function(){
